ec616aadf2b6903d0587336b6b619040b9fdb999
trxphousecsun <humberto.renderos.508@my.csun.edu>
2024-06-01 13:49:53 -0700

    Removed unecessary calls (pricing, register, login)

--- testapp/scripts.js
+++ testapp/scripts.js
@@ -7,26 +7,11 @@ mobileMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-// Scroll to pricing section
-document.querySelector('a[href="#pricing"]').addEventListener('click', (event) => {
-    event.preventDefault();
-    document.querySelector('h2:nth-of-type(4)').scrollIntoView({ behavior: 'smooth' });
-});
 
-// Scroll to waitlist section and center email input
-document.querySelector('a[href="#register"]').addEventListener('click', (event) => {
-    event.preventDefault();
-    const waitlistSection = document.querySelector('.waitlist');
-    waitlistSection.scrollIntoView({ behavior: 'smooth' });
-    waitlistSection.querySelector('.waitlist-input').focus();
-});
 
-document.querySelector('a[href="#login"]').addEventListener('click', (event) => {
-    event.preventDefault();
-    const waitlistSection = document.querySelector('.waitlist');
-    waitlistSection.scrollIntoView({ behavior: 'smooth' });
-    waitlistSection.querySelector('.waitlist-input').focus();
-});
+
+
+
 
 // Scroll to top when logo is clicked
 logoBtn.addEventListener('click', () => {
